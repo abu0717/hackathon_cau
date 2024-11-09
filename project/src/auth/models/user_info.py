1c580b32a20be45a2c61ea4bba4edfa8dd3e680e
abu0717 <nosirovabdulloh952@gmail.com>
2024-11-09 12:54:11 +0500

    [new] user_info post and get views

--- project/src/auth/models/user_info.py
+++ project/src/auth/models/user_info.py
@@ -1,25 +1,19 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database.models import BaseModel
-from sqlalchemy import ForeignKey, Enum
-from sqlalchemy import String
-from src.database.types import str_64, str_256
-from src.auth.manager import UserManager
-from enum import Enum as GEnum
-
-
-class Gender(GEnum):
-    Male = "M"
-    Female = "F"
-
+from sqlalchemy import ForeignKey
+from src.database.types import str_64
+from datetime import datetime
 
 class UserInfo(BaseModel):
     __tablename__ = "user_info"
 
-    full_name: Mapped[str_64]
-    age: Mapped[int]
-    gender: Mapped[Gender] = mapped_column(Enum(Gender), nullable=False)
     weight: Mapped[float]
     height: Mapped[float]
+    chest_size: Mapped[float] = mapped_column(nullable=True)
+    waist_size: Mapped[float] = mapped_column(nullable=True)
+    hips_size: Mapped[float] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+
     user_id: Mapped[int] = mapped_column(ForeignKey('accounts.id', ondelete="CASCADE"))
 
     def __str__(self):
--- project/src/auth/views.py
+++ project/src/auth/views.py
@@ -5,8 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from src.database.database import get_session
-from .schemas import Token, User, UserInDB
-from .models import AccountModel, SessionModel
+from .schemas import Token, User, UserInDB, UserInfoSchema
+from .models import AccountModel, SessionModel, UserInfo
 from .manager import credentials_exception, oauth2_scheme, UserManager
 
 router = APIRouter(prefix='/account', tags=['account'])
@@ -14,8 +14,8 @@ router = APIRouter(prefix='/account', tags=['account'])
 
 @router.post("/token", response_model=Token)
 async def login(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    session: AsyncSession = Depends(get_session)
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        session: AsyncSession = Depends(get_session)
 ) -> Token:
     user: AccountModel | None = (await session.execute(
         select(AccountModel).filter(AccountModel.username == form_data.username)
@@ -34,16 +34,16 @@ async def login(
 
 @router.post("/refresh", response_model=Token)
 async def refresh(
-    token: Annotated[str, Depends(oauth2_scheme)], session: Annotated[AsyncSession, Depends(get_session)]
+        token: Annotated[str, Depends(oauth2_scheme)], session: Annotated[AsyncSession, Depends(get_session)]
 ):
-  access_token ,= await UserManager.refresh_access_token(token, session)
-  return Token(access_token=access_token, token_type="bearer", refresh_token=token)
-
+    access_token, = await UserManager.refresh_access_token(token, session)
+    return Token(access_token=access_token, token_type="bearer", refresh_token=token)
 
 
 @router.post('/')
 async def create_account(response: Response, data: UserInDB = Body(), session: AsyncSession = Depends(get_session)):
-    user = AccountModel(username=data.username, password=AccountModel.get_password_hash(data.password), phone=data.phone)
+    user = AccountModel(username=data.username, password=AccountModel.get_password_hash(data.password),
+                        phone=data.phone)
     try:
         session.add(user)
         await session.commit()
@@ -68,10 +68,49 @@ async def create_account(response: Response, data: UserInDB = Body(), session: A
 
 @router.get("/", response_model=User)
 async def get_me(
-    user_session: SessionModel = Depends(AccountModel.get_current_user),
+        user_session: SessionModel = Depends(AccountModel.get_current_user),
 ):
     if user_session.active:
         return User(username=user_session.user.username, phone=user_session.user.phone)
     raise HTTPException(status_code=400, detail="Inactive user")
 
 
+@router.post('/user_info')
+async def user_info(user: UserInfoSchema, session: AsyncSession = Depends(get_session),
+                    user_id: int = Depends(AccountModel.get_current_user)):
+    new_user_info = UserInfo(
+        weight=user.weight,
+        height=user.height,
+        chest_size=user.chest_size,
+        waist_size=user.waist_size,
+        hips_size=user.hips_size,
+        user_id=user_id
+    )
+    session.add(new_user_info)
+    await session.commit()
+    await session.refresh(new_user_info)
+
+    return {"message": "User Info updated successfully", "user_info": new_user_info}
+
+
+@router.get("/user_info")
+async def get_user_info(session: AsyncSession = Depends(get_session)):
+    try:
+        stmt = select(UserInfo)
+        result = await session.execute(stmt)
+        user_info = result.scalars().all()
+        if not user_info:
+            return {"message": "No user info found"}
+        return [
+            {
+                "weight": u.weight,
+                "height": u.height,
+                "chest_size": u.chest_size,
+                "waist_size": u.waist_size,
+                "hips_size": u.hips_size,
+                "created_at": u.created_at
+            }
+            for u in user_info
+        ]
+    except Exception as e:
+        return {"error": str(e)}
