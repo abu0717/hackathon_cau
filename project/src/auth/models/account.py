a3f6417ddfdd7847dcbab06cd6d6952084d24cec
Asadbek <ahikmatullayev024@gmail.com>
2024-11-09 23:12:10 +0500

    [fix] birth_date -> accounts

--- project/src/auth/models/account.py
+++ project/src/auth/models/account.py
@@ -3,6 +3,7 @@ from src.database.models import BaseModel
 from sqlalchemy import String
 from src.database.types import str_64, str_256
 from src.auth.manager import UserManager
+from datetime import date
 
 
 class AccountModel(BaseModel, UserManager):
@@ -12,6 +13,7 @@ class AccountModel(BaseModel, UserManager):
     password: Mapped[str_256]
     phone: Mapped[str] = mapped_column(String(length=13), unique=True)
     sessions: Mapped[list["SessionModel"]] = relationship(back_populates='user')
+    birth_date: Mapped[date]
 
     def __str__(self):
         return f"<AccountModel: (username={self.username}, phone={self.phone})>"
